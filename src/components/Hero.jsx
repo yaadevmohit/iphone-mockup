e874c31ad93b466ec91714fa41ede2f25e4f7d90
yaadevmohit <mohityadav530@gmail.com>
2024-09-19 14:09:32 -0400

    update higlights

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
 
-    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 620 ? smallHeroVideo : heroVideo);
     
     function handleVideoSrcSet() {
-        window.innerWidth < 760 ? setVideoSrc(smallHeroVideo) : setVideoSrc(heroVideo)
+        window.innerWidth < 620 ? setVideoSrc(smallHeroVideo) : setVideoSrc(heroVideo)
     }
     
     useEffect(() => {
@@ -21,20 +21,36 @@ const Hero = () => {
     useGSAP(() => {
         gsap.to('#hero', {
             opacity: 1,
-            delay: 1.5,
+            delay: 2,
+        }),
+        gsap.to('#cta', {
+            y: -50,
+            opacity: 1,
+            delay: 2,
         })
-        }, 
-    [])
+        
+    }, [])
 
     return (
         <section className="w-full nav-height bg-black relative">
             <div className="h-5/6 w-full flex-center flex-col">
                 <p id="hero"className="hero-title">iPhone 15 Pro</p>
-                <div className="md:w-10/12 w-9/12">
-                    <video autoPlay muted playsInline={true} key={videoSrc}>
+                <div className="w-9/12 md:w-10/12">
+                    <video className="pointer-events-none"autoPlay muted playsInline={true} key={videoSrc}>
                         <source src={videoSrc}/>
                     </video>
                 </div>
+            </div>
+
+            <div 
+                id="cta"
+                className="flex flex-col items-center opacity-0
+                translate-y-20"
+            >
+                <a href="#highlights" className="btn">Buy</a>
+                <p className="font-normal text-xl">
+                    From $199/mo. or $999
+                </p>
             </div>
         </section>
     );
